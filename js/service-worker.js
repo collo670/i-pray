a7f991d27e8ea242914b4c636df1b43b052db1da
collo670 <otienocollo95@gmail.com>
2026-08-01 11:57:34 +0000

    service worker update

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipray-v202607141200';
+const CACHE_NAME = 'ipray-v202608011233';
 
 // Critical app shell: cached during install. If any of these fail, the
 // install fails, so keep this list short and only list files that exist.
